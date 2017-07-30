1ffbe7c4fe3d05b515ba936a40ca3ab126fbfb7c
Mike Lambert <lambert8@illinois.edu>
2017-07-30 04:38:28 -0500

    Mount in a hostPath for the mongo data

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@
 #
 # Usage: ./start.sh
 set -e
-docker start mongo || docker run -it -d --net=host -p 27017:27017 --name=mongo --restart=Always mongo || exit 1
+docker start mongo || docker run -it -d --net=host -p 27017:27017 --name=mongo -v $(pwd)/db:/data/db --restart=Always mongo || exit 1
 docker start flask || docker run -it -d --link=mongo --name=flask --restart=Always -p 8080:8080 upstaging/apiserver
